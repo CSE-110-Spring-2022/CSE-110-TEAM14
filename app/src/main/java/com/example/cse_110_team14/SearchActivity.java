fbd71b34bdea14266c70531c532f3d023caaa51a
tsugita26 <tsugita@ucsd.edu>
2022-04-29 15:27:20 -0700

    Show default search list

--- app/src/main/java/com/example/cse_110_team14/SearchActivity.java
+++ app/src/main/java/com/example/cse_110_team14/SearchActivity.java
@@ -1,20 +1,36 @@
 package com.example.cse_110_team14;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class SearchActivity extends AppCompatActivity {
 
+    public RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+        SearchListAdapter adapter = new SearchListAdapter();
+        adapter.setHasStableIds(true);
+
+        recyclerView = findViewById(R.id.search_items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+
 
         // Example of animal Object:
         /*
@@ -33,9 +49,12 @@ public class SearchActivity extends AppCompatActivity {
         // Map from animal id to animal object
         Map<String, ZooData.VertexInfo> animalMap =
                 ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
+
+
         // List of animals
         List<ZooData.VertexInfo> animalList = new ArrayList<>(animalMap.values());
 
+        adapter.setSearchItems(animalList);
 
     }
 }
