d691d28018f853e29fe66ce75903124ca2c0c9d8
Yingkai <yil179@ucsd.edu>
2022-05-04 00:38:24 -0700

    plan button count and plan activity

--- app/src/main/java/com/example/cse_110_team14/SearchListAdapter.java
+++ app/src/main/java/com/example/cse_110_team14/SearchListAdapter.java
@@ -59,6 +59,7 @@ public class SearchListAdapter extends RecyclerView.Adapter<SearchListAdapter.Vi
         return searchItems.size();
     }
 
+    // I changed this to non static
     public class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView textView;
         private ZooData.VertexInfo searchItem;
