a143fef5773900954785e2e45611fc4b4a54f98c
Akil Selvan Rajendra Janarthanan <arajendr@ucsd.edu>
2022-05-05 17:25:28 -0700

    Finished #9

--- app/src/main/java/com/example/cse_110_team14/PlanActivity.java
+++ app/src/main/java/com/example/cse_110_team14/PlanActivity.java
@@ -2,6 +2,7 @@ package com.example.cse_110_team14;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Pair;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -42,35 +43,114 @@ public class PlanActivity extends AppCompatActivity {
         }
         Graph<String, IdentifiedWeightedEdge> g
                 = ZooData.loadZooGraphJSON(this, "sample_zoo_graph.json");
-        shortestPath(plannedAnimalsIds, g);
-    }
 
-    // This method finds the order of vertexes that the user should visit
-    public void shortestPath(ArrayList<String> plannedAnimals,
-                             Graph<String, IdentifiedWeightedEdge> g) {
-        String start = "entrance_exit_gate";
-        String goal = null;
-        for (int i = 0; i < plannedAnimals.size(); i++) {
-            goal = plannedAnimals.get(i);
-            shortestPathHelper(start, goal, g);
+        Pair<List<GraphPath<String, IdentifiedWeightedEdge>>,List<String>> truePathPair =
+                shortestPath(plannedAnimalsIds, g,
+                        "entrance_exit_gate", "entrance_exit_gate");
+
+        List<GraphPath<String, IdentifiedWeightedEdge>> truePath = truePathPair.first;
+        List<String> truePathNames = truePathPair.second;
+
+        for(int i = 0; i < truePath.size(); i++) {
+            GraphPath<String, IdentifiedWeightedEdge> path = truePath.get(i);
+
+            Log.d("PlanActivity",
+                    edgeListToString(g,path, truePathNames.get(i), truePathNames.get(i+1)));
         }
     }
 
-    private void shortestPathHelper(String start, String goal,
+    // This method finds the order of vertexes that the user should visit
+    public Pair<List<GraphPath<String, IdentifiedWeightedEdge>>,List<String>>
+                            shortestPath(ArrayList<String> plannedAnimals,
+                                         Graph<String, IdentifiedWeightedEdge> g,
+                                         String start, String goal) {
+
+        List<String> visited = new ArrayList<>();
+        List<GraphPath<String, IdentifiedWeightedEdge>> truePath  = new ArrayList<>();
+        visited.add(start);
+        while(plannedAnimals.size() > 0) {
+            List<Integer> possiblePathLengths = new ArrayList<>();
+            List<String> possiblePaths = new ArrayList<>();
+            List<GraphPath<String, IdentifiedWeightedEdge>> possiblePathsList = new ArrayList<>();
+            for (int i = 0; i < plannedAnimals.size(); i++) {
+                String goal2 = plannedAnimals.get(i);
+                GraphPath<String, IdentifiedWeightedEdge> path = shortestPathHelper(start, goal2, g);
+                int length = pathLength(g, path);
+                possiblePathLengths.add(length);
+                possiblePaths.add(goal2);
+                possiblePathsList.add(path);
+            }
+            int min = Integer.MAX_VALUE;
+            int index = 0;
+            for (int i = 0; i < possiblePathLengths.size(); i++) {
+                if (possiblePathLengths.get(i) < min) {
+                    min = possiblePathLengths.get(i);
+                    index = i;
+                }
+            }
+            String goal2 = possiblePaths.get(index);
+            plannedAnimals.remove(goal2);
+            start = goal2;
+            truePath.add(possiblePathsList.get(index));
+            visited.add(goal2);
+        }
+
+        truePath.add(shortestPathHelper(start, goal, g));
+        visited.add(goal);
+
+        return new Pair<>(truePath, visited);
+    }
+
+    private GraphPath<String, IdentifiedWeightedEdge> shortestPathHelper(String start, String goal,
                                     Graph<String, IdentifiedWeightedEdge> g) {
         GraphPath<String, IdentifiedWeightedEdge> path =
                 DijkstraShortestPath.findPathBetween(g, start, goal);
-        // 2. Load the information about our nodes and edges...
+
+        return path;
+    }
+
+    public String edgeListToString(Graph<String, IdentifiedWeightedEdge> g,
+                                   GraphPath<String, IdentifiedWeightedEdge> path,
+                                   String start, String goal) {
+
+        StringBuilder sb = new StringBuilder();
+//        sb.append("The shortest path from " +
+//                animalIdToName.get(start) + " to " +
+//                animalIdToName.get(goal) + " is:\n");
         Map<String, ZooData.VertexInfo> vInfo =
                 ZooData.loadVertexInfoJSON(this, "sample_node_info.json");
         Map<String, ZooData.EdgeInfo> eInfo =
                 ZooData.loadEdgeInfoJSON(this, "sample_edge_info.json");
-        Log.d("PlanActivity", "The shortest path from '" + animalIdToName.get(start) + " to '" +
-                animalIdToName.get(goal) + " is:\n");
         int i = 1;
+        String current = null;
         for (IdentifiedWeightedEdge e : path.getEdgeList()) {
-            Log.d("PlanActivity", "  " + i + ". Walk " + g.getEdgeWeight(e) + " meters along " + eInfo.get(e.getId()).street + " from " + vInfo.get(g.getEdgeSource(e).toString()).name + " to " + vInfo.get(g.getEdgeTarget(e).toString()).name);
-            i++;
+            sb.append(" " + i + ". ");
+            if(eInfo.get(e.getId()).street.equals(current)) {
+                sb.append("Continue on " + eInfo.get(e.getId()).street);
+            }
+            else {
+                sb.append("Proceed on  " + eInfo.get(e.getId()).street);
+            }
+            current = eInfo.get(e.getId()).street;
+//            sb.append(" " + i + ".Walk " + g.getEdgeWeight(e) + " meters along " +
+//                    eInfo.get(e.getId()).street + " from " +
+//                    street1 + " to " +
+//                    street2 + "\n");
+//            i++;
+//            start = street2;
         }
+
+        return sb.toString();
     }
+    public int pathLength(Graph<String, IdentifiedWeightedEdge> g,
+                          GraphPath<String, IdentifiedWeightedEdge> path) {
+        int length = 0;
+        for (IdentifiedWeightedEdge e : path.getEdgeList()) {
+            length += g.getEdgeWeight(e);
+        }
+
+        return length;
+    }
+
+
 }
