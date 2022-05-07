233091df5a144d56413c23cbfcf00e34a75b5b80
Celino <yic057@ucsd.edu>
2022-05-06 19:04:21 -0700

    Added new activity and its elements

---
+++ app/src/main/java/com/example/cse_110_team14/VisitAnimalActivity.java
@@ -0,0 +1,14 @@
+package com.example.cse_110_team14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitAnimalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit_animal);
+    }
+}
